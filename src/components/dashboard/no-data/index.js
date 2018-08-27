e9477978c43e8d0e23ce1fd6425f9c6280326f6c
lapostoj <jerome.lapostolet@transferwise.com>
2018-08-27 20:24:09 +0100

    Move components to index files and rename css files

--- src/components/dashboard/no-data/index.js
+++ src/components/dashboard/no-data/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import UploadForm from './upload-form';
+import './no-data.css';
 import logo from './glass-and-bottle.svg';
 
 const NoData = ({onDataAdded}) => (
   <div className="center">
     <img src={logo} className="no-data-img" height="300" alt="bouteille-et-verre" />
     <h2>Pas de bouteille disponible</h2>
-    <div className="center">
-      <UploadForm onUpload={onDataAdded}></UploadForm>
-    </div>
+    <UploadForm onUpload={onDataAdded}></UploadForm>
   </div>
 );
 
--- src/components/dashboard/wine-list/index.js
+++ src/components/dashboard/wine-list/index.js
@@ -1,4 +1,41 @@
-import WineList from './WineList';
-import './WineList.css';
+import React from 'react';
+import Types from 'prop-types';
+import './wine-list.css';
+
+const WineList = ({ wines }) => {
+  const getClassForWine = (wine) => {
+    return wine.color.toLowerCase();
+  }
+
+  const renderWineRow = (wine, index) => {
+    return (
+      <li
+        key={index}
+        className={"wine-row " + getClassForWine(wine)}
+      >
+        <span>{wine.name}</span>
+      </li>
+    );
+  }
+
+  return (
+    <ul className="wine-list">
+      { wines.map(renderWineRow) }
+    </ul>
+  );
+}
+
+WineList.propTypes = {
+  wines: Types.arrayOf(
+    Types.shape({
+      name: Types.string.isRequired,
+      color: Types.string.isRequired,
+      year: Types.number,
+      type: Types.string,
+      size: Types.string,
+      quantity: Types.number,
+    }),
+  ).isRequired,
+};
 
 export default WineList;
