d8bc6541cdba51340f12fb7a2f2d500786826f39
lapostoj <jerome.lapostolet@gmail.com>
2019-08-26 10:56:38 +0100

    Fix act warning when running tests

--- src/components/dashboard/dashboard.test.js
+++ src/components/dashboard/dashboard.test.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { act } from 'react-dom/test-utils';
 import { mount } from 'enzyme';
 import { getBottlesForCellarId } from '../../clients/bottle-client';
 import { aBottle } from '../../test/wine.factory';
@@ -6,21 +7,27 @@ import Dashboard from '.';
 jest.mock('../../clients/bottle-client');
 
 describe('Dashboard', () => {
-  it('should display no data component if no wine is returned', () => {
+  it('should display no data component if no wine is returned', async () => {
     getBottlesForCellarId.mockImplementation(() => Promise.resolve([]));
 
-    const component = mount(<Dashboard />);
+    let component;
+    await act(async () => {
+      component = mount(<Dashboard />);
+    });
 
     expect(component.find('NoData')).toHaveLength(1);
     expect(component.find('WineList')).toHaveLength(0);
   });
 
-  it('should display wine list component if some wines are returned', () => {
+  it('should display wine list component if some wines are returned', async () => {
     getBottlesForCellarId.mockImplementation(() =>
       Promise.resolve([aBottle()])
     );
 
-    const component = mount(<Dashboard />);
+    let component;
+    await act(async () => {
+      component = mount(<Dashboard />);
+    });
 
     setImmediate(() => {
       component.update();
