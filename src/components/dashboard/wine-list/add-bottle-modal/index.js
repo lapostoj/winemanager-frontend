13fc2896fc488387b10cd497c24ad8f309e3a54d
lapostoj <jerome.lapostolet@gmail.com>
2020-05-10 08:59:41 +0100

    Improve add bottle form

--- src/components/dashboard/wine-list/add-bottle-modal/index.js
+++ src/components/dashboard/wine-list/add-bottle-modal/index.js
@@ -8,100 +8,104 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  FormControl,
-  InputLabel,
   MenuItem,
-  Select,
   TextField,
 } from '@material-ui/core';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 import { createBottle } from '../../../../clients/bottle-client';
 
+const BottleSizes = [
+  { value: 37, name: 'Demi-bouteille' },
+  { value: 50, name: 'Demi-litre' },
+  { value: 62, name: 'Jura' },
+  { value: 75, name: 'Bouteille' },
+  { value: 150, name: 'Magnum' },
+  { value: 300, name: 'Jéroboam' },
+  { value: 600, name: 'Mathusalem' },
+  { value: 1200, name: 'Salmanazar' },
+  { value: 1800, name: 'Balthazar' },
+  { value: 2400, name: 'Nabuchodonosor' },
+];
+
+const DefaultBottle = {
+  Year: 2019,
+  Size: 75,
+  Quantity: 1,
+  CellarID: 5662025548038144,
+  WineID: 5744378123386880,
+};
+
 export default function AddBottleModal({ open, close }) {
-  const bottleSizes = [
-    { value: 37, name: 'Demi-bouteille' },
-    { value: 50, name: 'Demi-litre' },
-    { value: 62, name: 'Jura' },
-    { value: 75, name: 'Bouteille' },
-    { value: 150, name: 'Magnum' },
-    { value: 300, name: 'Jéroboam' },
-    { value: 600, name: 'Mathusalem' },
-    { value: 1200, name: 'Salmanazar' },
-    { value: 1800, name: 'Balthazar' },
-    { value: 2400, name: 'Nabuchodonosor' },
-  ];
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const [bottle, setBottle] = useState({
-    Year: 2019,
-    Size: 75,
-    Quantity: 1,
-    CellarID: 5662025548038144,
-    WineID: 5744378123386880,
-  });
+  const [bottle, setBottle] = useState(DefaultBottle);
 
-  const addBottle = () => {
-    createBottle(bottle).then(() => {
-      close();
-    });
-  };
+  const addBottle = () => createBottle(bottle).then(close);
 
-  const handleNumberChange = (name) => (event) => {
+  const handleNumberChange = (name) => (event) =>
     setBottle({ ...bottle, [name]: Number(event.target.value) });
-  };
 
   return (
-    <Dialog open={open} onClose={close} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">Bouteille</DialogTitle>
+    <Dialog
+      open={open}
+      onClose={close}
+      maxWidth={false}
+      fullScreen={fullScreen}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle id="form-dialog-title">Ajouter une bouteille</DialogTitle>
       <DialogContent>
         <DialogContentText>
           Entrer les détails de la bouteille à ajouter.
         </DialogContentText>
         <Box mb={2}>
-          <form>
-            <Box display="inline-block" mr={2}>
-              <TextField
-                id="bottleYear"
-                type="number"
-                label="Année"
-                value={bottle.Year}
-                onChange={handleNumberChange('Year')}
-                autoFocus
-              />
-            </Box>
-            <Box display="inline-block" mr={2}>
-              <FormControl>
-                <InputLabel htmlFor="bottleSize">Bouteille</InputLabel>
-                <Select
-                  value={bottle.Size}
-                  onChange={handleNumberChange('Size')}
-                  inputProps={{
-                    id: 'bottleSize',
-                  }}
-                  renderValue={(value) =>
-                    `${
-                      bottleSizes.find(
-                        (bottleSize) => bottleSize.value === value
-                      ).name
-                    } - ${value}ml`
-                  }
-                >
-                  {bottleSizes.map((bottle, index) => (
-                    <MenuItem key={index} value={bottle.value}>
-                      {bottle.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-            <Box display="inline-block" mr={2}>
-              <TextField
-                id="bottleQuantity"
-                type="number"
-                label="Quantité"
-                value={bottle.Quantity}
-                onChange={handleNumberChange('Quantity')}
-              />
-            </Box>
-          </form>
+          <Box display="inline-block" m={2} width="250px">
+            <TextField
+              id="input-select-bottle-size"
+              label="Bouteille"
+              select
+              variant="outlined"
+              fullWidth={true}
+              value={bottle.Size}
+              onChange={handleNumberChange('Size')}
+              autoFocus
+              SelectProps={{
+                renderValue: (value) =>
+                  `${
+                    BottleSizes.find((bottleSize) => bottleSize.value === value)
+                      .name
+                  } - ${value}ml`,
+              }}
+            >
+              {BottleSizes.map((bottle, index) => (
+                <MenuItem key={index} value={bottle.value}>
+                  {bottle.name}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Box>
+          <Box display="inline-block" m={2}>
+            <TextField
+              id="bottleYear"
+              type="number"
+              label="Année"
+              variant="outlined"
+              value={bottle.Year}
+              onChange={handleNumberChange('Year')}
+            />
+          </Box>
+          <Box display="inline-block" m={2}>
+            <TextField
+              id="bottleQuantity"
+              type="number"
+              label="Quantité"
+              variant="outlined"
+              value={bottle.Quantity}
+              onChange={handleNumberChange('Quantity')}
+            />
+          </Box>
         </Box>
       </DialogContent>
       <DialogActions>
